acef115df54dad8c245799f22917b707cedb5876
iliyasahmedgit <iliyasrobin.social@icloud.com>
2022-04-22 10:48:10 +0600

    second changes

---
+++ app.js
@@ -0,0 +1,54 @@
+var text = '';
+function findMeal (){
+    text = document.getElementById('meal-search').value;
+    connectMeal(text)
+    document.getElementById('meal-search').value = '';
+}
+
+
+function connectMeal (text) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`)
+    .then(res=> res.json() )
+    .then(data=> loadMeal(data));
+
+}
+
+connectMeal();
+
+function loadMeal (data){
+
+  console.table(data.meals[0]);
+
+  var container = document.getElementById('main-container');
+  var idm = data.meals[0].idMeal;
+     var imgLink = data.meals[0].strMealThumb;
+     var cat = data.meals[0].strCategory;
+     var title = data.meals[0].strMeal;
+    var area = data.meals[0].strArea;
+    var ins = data.meals[0].strIngredient1 ;
+    var ins2 = data.meals[0].strIngredient2 ;
+    var ins3 = data.meals[0].strIngredient3 ;
+    var ins4 = data.meals[0].strIngredient4 ;
+    
+
+    
+   
+
+     container.innerHTML = `
+                        <p> <b> ID: ${idm}</b> </p>
+                        <p><b> Meal Name:  ${title} </b></p>
+                         <img width="300px" src="${imgLink}" <br/> <br> <br>
+                         <p> <b> Category: ${cat}</b> </p>
+                         <p> <b> Area: ${area}</b>
+                         <p> <b>Ingredients :<br> 
+                         1. ${ins} <br>
+                         2. ${ins2} <br>
+                         3. ${ins3} <br>
+                         4. ${ins4}</b>
+                        `;
+
+   
+
+    container.appendChild(newDiv);
+
+}
